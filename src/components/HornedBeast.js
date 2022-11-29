a153ebf55101541164881bdd3987038ab15f85e7
DEIOSHA SPARKS <DEIOSHASPARKS13@GMAIL.COM>
2022-11-29 09:58:48 -0800

    added changes to js files

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -3,12 +3,11 @@ import React from 'react';
 class HornedBeast extends React.Component {
   render() {
     return <div>
-    <h2>Puppies In a Basket</h2>
-    <img 
-    src="https://media.istockphoto.com/id/1333462174/photo/three-little-sleeping-puppies-of-welsh-corgi-pembroke-breed-dog-in-basket-at-nature.jpg?b=1&s=170667a&w=0&k=20&c=ydZeqd11ojzwZyfDczUHGZET0QcnIhRRR1vL-kIlUqU="
-    alt='three small puppies in a basket'
+    <h2>{this.props.title}</h2>
+    <img src={this.props.imageUrl}
+    alt={this.props.description}
     />
-    <p>This is three cute puppies sitting in a basket outside</p>
+    <p>{this.props.description}</p>
     </div>
   }
 }
--- src/components/main.js
+++ src/components/main.js
@@ -4,13 +4,16 @@ import HornedBeast from "./HornedBeast";
 class Main extends React.Component {
   render() {
     return <div>
-    <HornedBeast />
-    <h2>This is my title</h2>
+    <HornedBeast title="Puppies" description="three puppies" imageUrl="https://media.istockphoto.com/id/1333462174/photo/three-little-sleeping-puppies-of-welsh-corgi-pembroke-breed-dog-in-basket-at-nature.jpg?b=1&s=170667a&w=0&k=20&c=ydZeqd11ojzwZyfDczUHGZET0QcnIhRRR1vL-kIlUqU=" />
+    <HornedBeast title="Title2" description="for Puppies" imageUrl="https://media.istockphoto.com/id/1333462174/photo/three-little-sleeping-puppies-of-welsh-corgi-pembroke-breed-dog-in-basket-at-nature.jpg?b=1&s=170667a&w=0&k=20&c=ydZeqd11ojzwZyfDczUHGZET0QcnIhRRR1vL-kIlUqU=" />
+    {/* <h2>This is my title</h2>
     <img 
     src="https://www.princeton.edu/sites/default/files/styles/half_2x/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=iQEwihUn"
     alt="happy puppy"
     />
-    <p> This is my description: A page for puppies</p>
+    <p> This is my description: A page for puppies</p> */}
+
+
     
     </div>
   }

212e2ac72e9316f4c218ea6209e596fa0405c2b3
DEIOSHA SPARKS <DEIOSHASPARKS13@GMAIL.COM>
2022-11-30 21:50:00 -0800

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React from "react";
-import HornedBeast from "./components/HornedBeast.js";
 import hornedData from "./data.json";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Main from "./components/main.js";
+import SelectedBeast from './components/SelectedBeast';
+import Container from "react-bootstrap";
 
 
-function App() {
 
-  return (
-    <Container>
-      <Row lg={10} xs={5} sm={3} md={4}>
-        {hornedData.map(hornedInfo => (
-          <Col key={hornedInfo._id}>
-        <HornedBeast key={hornedInfo._id} title={hornedInfo.title} imageUrl={hornedInfo.image_url} />
-        </Col>))}
-      </Row>
-    </Container>
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allBeast: hornedData,
+      displayModal: false,
+      selectedBeast: {},
+    }
+  }
+
+  openModal = (name) => {
+    const clickedBeast = hornedData.find(beast => beast.title === name);
+    this.setState({
+      selectedBeast: clickedBeast,
+      displayModal: true,
+    });
+  }
+  hideModal = () => {
+    this.setState({ selectedBeast: false });
+  }
+
+
+  render() {
+
+    return (
+      <>
+        {/* // <Container > */}
+        <Main allBeast={this.state.allBeast} displayModal={this.state.displayModal}/>
+      <SelectedBeast
+          hideModal={this.hideModal}
+          showModal={this.state.displayModal}
+          beast={this.state.selectedBeast}
+          />
+      {/* </Container > */}
+      </>
+
   );
+  }
 }
-
 export default App;
--- src/components/main.js
+++ src/components/main.js
@@ -1,15 +1,25 @@
-// import React from "react";
-// import HornedBeast from "./HornedBeast";
+import React from "react";
+import HornedBeast from "./HornedBeast";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-// class Main extends React.Component {
-//   render() {
-//     return <div>
-    {/* <HornedBeast title="Puppies" description="three puppies" imageUrl="https://media.istockphoto.com/id/1333462174/photo/three-little-sleeping-puppies-of-welsh-corgi-pembroke-breed-dog-in-basket-at-nature.jpg?b=1&s=170667a&w=0&k=20&c=ydZeqd11ojzwZyfDczUHGZET0QcnIhRRR1vL-kIlUqU=" />
-    <HornedBeast title="Title2" description="for Puppies" imageUrl="https://media.istockphoto.com/id/1333462174/photo/three-little-sleeping-puppies-of-welsh-corgi-pembroke-breed-dog-in-basket-at-nature.jpg?b=1&s=170667a&w=0&k=20&c=ydZeqd11ojzwZyfDczUHGZET0QcnIhRRR1vL-kIlUqU=" /> */}
-    
-//     </div>
-//   }
-// }
+class Main extends React.Component {
+  
+    render() {
+        return (
+            <Container>
+                <Row lg={10} xs={5} sm={3} md={4}>
+                    {this.props.allBeast.map(hornedInfo => (
+                        <Col key={hornedInfo._id}>
+                            <HornedBeast key={hornedInfo._id} title={hornedInfo.title} imageUrl={hornedInfo.image_url} description={hornedInfo.description} />
+                        </Col>))}
+                </Row>
+            </Container>
+        )
+
+    }
+}
 
 
 
